28f3879bf54dc7fe8a8dd704adda2312f48e6362
Bram Janssens <s.a.janssens@gmail.com>
2025-09-18 09:55:16 +0200

    Refactor Person class constructors to use constructor chaining.

--- src/main/java/com/infosupport/oo/Person.java
+++ src/main/java/com/infosupport/oo/Person.java
@@ -4,6 +4,8 @@ import java.time.LocalDate;
 import java.time.Period;
 import java.util.Objects;
 
+import static java.lang.IO.println;
+import static java.time.LocalDate.EPOCH;
 import static java.time.LocalDate.now;
 
 public class Person { // POJO
@@ -16,6 +18,25 @@ public class Person { // POJO
 
     // CONSTRUCTOR
 
+    public Person() {
+        // WET:
+        // this.firstName = "Unknown";
+        // this.lastName = "Unknown";
+        // this.dateOfBirth = LocalDate.EPOCH;
+
+        // vervangen door:
+        // DRY:
+        this("Unknown", "Unknown", EPOCH); // constructor chaining
+    }
+
+    public Person(String firstName, String lastName) {
+        // this.firstName = firstName;
+        // this.lastName = lastName;
+        // this.dateOfBirth = LocalDate.EPOCH;
+        // vervangen door:
+        this(firstName, lastName, EPOCH);
+    }
+
     public Person(String firstName, String lastName, LocalDate dateOfBirth) {
         this.firstName = firstName;
         this.lastName = lastName;
@@ -24,21 +45,23 @@ public class Person { // POJO
 
     public int loop() {
         if (Period.between(this.dateOfBirth, now()).getYears() < 40) {
-            IO.println("Gaat lekker...");
+            println("Gaat lekker...");
             return 13;
         }
 
-        IO.println("HIJG HIJG");
+        println("HIJG HIJG");
         return 8;
     }
 
-    @Override public boolean equals(Object o) {
+    @Override
+    public boolean equals(Object o) {
         if (o == null || getClass() != o.getClass()) return false;
         Person person = (Person) o;
         return Objects.equals(firstName, person.firstName) && Objects.equals(lastName, person.lastName) && Objects.equals(dateOfBirth, person.dateOfBirth);
     }
 
-    @Override public int hashCode() {
+    @Override
+    public int hashCode() {
         return Objects.hash(firstName, lastName, dateOfBirth);
     }
 
@@ -46,7 +69,8 @@ public class Person { // POJO
         return lastName;
     }
 
-    @Override public String toString() {
+    @Override
+    public String toString() {
         return "Person{" +
                 "firstName='" + firstName + '\'' +
                 ", lastName='" + lastName + '\'' +
