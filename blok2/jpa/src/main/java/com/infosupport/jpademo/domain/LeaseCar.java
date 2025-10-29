d5c20d1c4865c4d88b47f3de677080f551dea1ab
Bram Janssens <s.a.janssens@gmail.com>
2025-10-29 12:51:26 +0100

    Add @GeneratedValue annotation to LeaseCar entity for ID generation

--- blok2/jpa/src/main/java/com/infosupport/jpademo/domain/LeaseCar.java
+++ blok2/jpa/src/main/java/com/infosupport/jpademo/domain/LeaseCar.java
@@ -2,6 +2,7 @@ package com.infosupport.jpademo.domain;
 
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import jakarta.persistence.OneToOne;
 import lombok.AllArgsConstructor;
@@ -9,12 +10,14 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import static jakarta.persistence.GenerationType.IDENTITY;
+
 @Data @NoArgsConstructor
 @Builder @AllArgsConstructor
 @Entity
 public class LeaseCar {
 
-    @Id // @GeneratedValue? no, since we use @MapsId and the personId is FK and PK
+    @Id @GeneratedValue(strategy = IDENTITY)
     private long id;
     private String brand;
 
